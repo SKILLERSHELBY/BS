84af1e50e174f0fd682ef1d8485e055cb98a073d
SKILLERSHELBY <joao92642598@gmail.com>
2021-03-19 08:50:28 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,26 @@
+### SKILLER NÉ KARALHO
+
+## Note : TRADUZINDO PELO ZEUS, SE FOR EDITAR DEIXE OS CRÉDITOS!
+
+
+## Install
+
+
+```bash
+> termux-setup-storage
+(after that tap on permission)
+> apt install git -y
+> git clone https://github.com/SKILLERSHELBY/BS.git
+> cd BS
+> bash install.sh
+```
+
+## Usage
+
+```bash
+> npm start
+```
+
+
+CRÉDITOS
+- YouTube: https://youtube.com/channel/UCpS0ngad4LjtDoux_JvOK6Q
---
+++ database/menu/help.js
@@ -0,0 +1,113 @@
+const help = (pushname, prefix, botName, ownerName, reqXp, uangku) => {
+        return `
+╔══✪〘 Informações 〙✪══
+║
+║───────⊹⊱✫⊰⊹───────
+║➩ ❍ SKILLER
+║➩ ❍ Prefix: 「  ${prefix}  」
+║➩ ❍ Criador: ${botName}
+║➩ ❍ Nome: ${pushname}️
+║➩ ❍ XP: ${reqXp}
+║➩ ❍ Money: ${uangku}
+───────⊹⊱✫⊰⊹───────
+
+
+❍ 𝙻𝙸𝙼𝙸𝚃𝙴
+
+  ║➩ ❍ ${prefix}limite (ver seu limite)
+  ║➩ ❍ ${prefix}buylimite (comprar limite)
+
+  ║➩ ❍ ${prefix}
+❍ 𝚂𝙾𝙱𝚁𝙴
+  
+  ║➩ ❍ ${prefix}info
+  ║➩ ❍ ${prefix}blocklist
+  ║➩ ❍ ${prefix}chatlist
+  ║➩ ❍ ${prefix}ping
+❍ 𝙵𝙰𝚉𝙴𝚁
+  
+  ║➩ ❍ ${prefix}figu
+  ║➩ ❍ ${prefix}toimg
+  ║➩ ❍ ${prefix}virarmp3
+ 
+❍ 𝙼𝙴𝙳𝙸𝙰
+ 
+  ║➩ ❍ ${prefix}ytsearch
+
+❍ 𝙳𝙾𝚆𝙽𝙻𝙾𝙰𝙳
+  
+  ║➩ ❍ ${prefix}images
+  ║➩ ❍ ${prefix}ytmp3
+  ║➩ ❍ ${prefix}ytmp4
+  ║➩ ❍ ${prefix}tiktok
+
+❍ 𝙼𝙴𝙼𝙴
+  
+  ║➩ ❍ ${prefix}meme
+  ║➩ ❍ ${prefix}memeindo
+
+❍ 𝚂𝙾𝙼
+  
+  ║➩ ❍ ${prefix}musica
+  ║➩ ❍ ${prefix}audio
+
+❍ 𝙰𝙽𝙸𝙼𝙴𝚂
+  
+  ║➩ ❍ ${prefix}neonime
+  ║➩ ❍ ${prefix}pokemon
+  ║➩ ❍ ${prefix}loli
+  ║➩ ❍ ${prefix}waifu
+  ║➩ ❍ ${prefix}randomanime
+  ║➩ ❍ ${prefix}husbu
+  ║➩ ❍ ${prefix}husbu2
+  ║➩ ❍ ${prefix}buscanime
+  ║➩ ❍ ${prefix}nekonime
+
+❍ 𝙳𝙾𝙽𝙾
+  
+  ║➩ ❍ ${prefix}setprefix
+  ║➩ ❍ ${prefix}block
+  ║➩ ❍ ${prefix}tm
+  ║➩ ❍ ${prefix}tmctt
+  ║➩ ❍ ${prefix}clone
+  ║➩ ❍ ${prefix}clearall
+
+❍ 𝙾𝚄𝚃𝚁𝙾𝚂
+ 
+  ║➩ ❍ ${prefix}send
+  ║➩ ❍ ${prefix}wame
+  ║➩ ❍ ${prefix}exe
+  ║➩ ❍ ${prefix}qrcode
+  ║➩ ❍ ${prefix}afk
+  ║➩ ❍ ${prefix}timer
+  ║➩ ❍ ${prefix}fml
+  ║➩ ❍ ${prefix}fml2
+  ❍𝙼𝙴𝙽𝚄 𝙰𝙳𝙼𝙸𝙽
+  
+ ║➩ ❍ ${prefix}abrirgp
+ ║➩ ❍ ${prefix}fechargp
+ ║➩ ❍ ${prefix}setname
+ ║➩ ❍ ${prefix}setdesc
+ ║➩ ❍ ${prefix}promover
+ ║➩ ❍ ${prefix}rebaixar
+ ║➩ ❍ ${prefix}marcar
+ ║➩ ❍ ${prefix}marcar2
+ ║➩ ❍ ${prefix}marcar3
+ ║➩ ❍ ${prefix}marcar4
+ ║➩ ❍ ${prefix}marcar5
+ ║➩ ❍ ${prefix}add
+ ║➩ ❍ ${prefix}remover
+ ║➩ ❍ ${prefix}listadmins
+ ║➩ ❍ ${prefix}linkgp
+ ║➩ ❍ ${prefix}sairgp
+ ║➩ ❍ ${prefix}bv
+ ║➩ ❍ ${prefix}nsfw
+ ║➩ ❍ ${prefix}leveling
+ ║➩ ❍ ${prefix}level
+ ║➩ ❍ ${prefix}apagar
+ ║➩ ❍ ${prefix}simih
+❍ 𝙼𝙴𝙽𝚄 𝙽𝚂𝙵𝚆
+ DESATIVEI
+`
+}
+exports.help = help
